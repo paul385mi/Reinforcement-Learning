e306fd483722cdff547a93193fe3bc9b6ea655bb
Paul Mill <paul.mill@web.de>
2025-06-19 11:29:58 +0200

    updated

---
+++ run_training.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -J ppo-training
+#SBATCH -p scc-gpu
+#SBATCH -n 1
+#SBATCH -c 32
+#SBATCH --gpus=1
+#SBATCH -t 2-00:00:00
+#SBATCH -o logs/output_%j.log
+#SBATCH -e logs/error_%j.log
+
+source ~/myenv/bin/activate  # falls du eine virtuelle Umgebung nutzt
+python3 train_gym_ppo.py --episodes 5000
